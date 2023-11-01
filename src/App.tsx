b59528c8c6c41858f24c5338df751537e744e1dd
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-11-01 17:12:09 +0600

    'Add a few picture file for project. This is a cheese.png, bacon.png, meet.png, salat.png'

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css';
 
 function App() {
+
+
+
     return (
       <div className='main-box'>
         <h3 className='title-burger'>ᗰᗩKE YOᑌᖇ ᗷEᔕT ᗷᑌᖇGEᖇ!</h3>
         <div className='box-items'>
+
           <div className='ingridients-box'>
             <span>Ingridients</span>
+
           </div>
 
           <div className='visualizer-burger-box'>
             <span>Burger</span>
           </div>
+
         </div>
       </div>
     );
